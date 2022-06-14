7c96a42ea7c81364853388268d4dad21f2cd4d1c
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-06-14 15:49:57 +0530

    first commit

---
+++ src/api/handlers/Order.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Api\Handlers;
+use Phalcon\Http\Response;
+use Phalcon\Di\Injectable;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class Order extends Injectable
+{
+    function placeorder()
+    {
+        if ($this->request->isPost()) {
+            $token = $this->request->getQuery('token');
+            $key = 'example_key';
+            $decodedtoken = JWT::decode($token, new Key($key, 'HS256'));
+
+            $data = array(
+                "customer_id" => $decodedtoken->id,
+                "customer_name" => $this->request->getPost('customer_name'),
+                "product_name" => $this->request->getPost('product_name'),
+                "product_quantity" => $this->request->getPost('quantity'),
+                "status" => "paid"
+            );
+
+            $this->mongo->order->insertOne($data);
+        }
+    }
+
+    function updateorder()
+    {
+        if ($this->request->isPut()) {
+            $token = $this->request->getQuery('token');
+            $key = 'example_key';
+            $decodedtoken = JWT::decode($token, new Key($key, 'HS256'));
+            $updated_status = $this->request->getPut('status');
+            $id = $this->request->getPut('id');
+
+            $this->mongo->order->updateOne(["_id" => new \MongoDB\BSON\ObjectID($id)], ['$set' => ['status'=> $updated_status]]);
+        }
+    }
+}
